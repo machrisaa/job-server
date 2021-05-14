7c69c3d60b1c4cc75d6866497c22ad64dd0c4dfb
Chris Yeung <chris.yeung@shearwatersystems.com>
2021-05-14 08:41:25 +0100

    added ALLoWED_HOSTS env variable

--- jobserver/settings.py
+++ jobserver/settings.py
@@ -41,7 +41,8 @@ BASE_URL = env.str("BASE_URL", default="http://localhost:8000")
 
 GRAPHNET_BACKEND_TOKEN = env.str("GRAPHNET_BACKEND_TOKEN")
 
-ALLOWED_HOSTS = [furl(BASE_URL).host]
+ALLOWED_HOSTS = [furl(BASE_URL).host, "host.docker.internal", 'localhost', *env.str('ALLOWED_HOSTS', '127.0.0.1').split(",")]
+print('ALLOWED_HOSTS:', ALLOWED_HOSTS)
 
 
 # Application definition
